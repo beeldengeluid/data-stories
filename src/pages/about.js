916a5d89ff23b59ddc638517a7d883eb87259878
Philo van Kemenade <philovankemenade@gmail.com>
2020-04-14 13:35:07 +0200

    update about text

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,22 +11,26 @@ const BlogIndex = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <SEO title="About" />
       <p>
-        The CLARIAH Media suite gives access to important Dutch multimedia
-        archives, among others: DANS oral history interview collections, Eye
-        Film Institute, Meertens Institute, National Library, Netherlands
-        Institute for Sound and Vision, Open Images Project.
+        The{" "}
+        <a href="https://mediasuite.clariah.nl/" target="_blank">
+          CLARIAH Media Suite
+        </a>{" "}
+        gives researchers access to Dutch multimedia archives from, among
+        others, Netherlands Institute for Sound and Vision, Eye Film Institute,
+        DANS, Meertens Institute and National Library.
       </p>
+
       <p>
-        The Media Suite tools offer the core functionalities needed for
-        performing scholarly research tasks with audio-visual media and
-        contextual collections. Tools available in this version of the Media
-        Suite enable metadata inspection, exploratory browsing, search,
-        visualization analysis (annotation support), and speech recognition.
+        The Media Suite tools offer the core functionalities needed for research
+        with these data and to generate “data stories” based on interesting
+        facts and figures that are extracted from these data.
       </p>
+
       <p>
-        Anyone can access the Media Suite for public domain material.
-        Researchers with a login at a Dutch university are allowed to explore
-        the entire collections.
+        Media Suite Data Stories serves as a platform for presenting various
+        types of such data stories. They can be part of a research topic that
+        scholars are working on in the Media Suite, the outcome of a Data-A-Thon
+        with students, or inspired by events in the news.
       </p>
     </Layout>
   )
