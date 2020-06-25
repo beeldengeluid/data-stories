d8498b5c3696f44e7b358efef720f0e2df9f8a34
Philo van Kemenade <philovankemenade@gmail.com>
2020-06-25 16:51:14 +0200

    update logo

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import { jsx } from "theme-ui"
 import { Link } from "gatsby"
 import "./layout.css"
 import Hero from "../components/hero"
-import logo from "../../content/assets/mediasuite-logo.svg"
+import logo from "../../content/assets/logo-media_suite.png"
 
 const Layout = ({ location, title, children }) => {
   return (
@@ -22,8 +22,8 @@ const Layout = ({ location, title, children }) => {
             src={logo}
             alt="Media Suite Logo"
             sx={{
-              width: "200px",
-              mb: "-12px",
+              width: 5,
+              margin: 0,
             }}
           />
         </Link>
