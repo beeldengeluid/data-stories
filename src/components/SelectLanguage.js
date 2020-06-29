d117d21a34133c6fee75f4a4a3b6d9e02782de0f
Philo van Kemenade <philovankemenade@gmail.com>
2020-06-29 17:58:33 +0200

    style SelectLanguage component

--- src/components/SelectLanguage.js
+++ src/components/SelectLanguage.js
@@ -1,20 +1,30 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
 import React from 'react';
 import Link from 'gatsby-link';
 
 const SelectLanguage = (props) => {
-  // console.log(props.langs);
+  console.log(props.langs);
   const links = props.langs.map(lang =>
-    <Link to={lang.link} key={lang.langKey} style={{
-      color: 'white'
+    <Link to={lang.link} key={lang.langKey} sx={{
+      color: 'muted',
+      textDecoration: 'none',
     }}>
-      <li selected={lang.selected}>
-        {lang.langKey}
+      <li sx={{
+        my: 2,
+        mx: [2, 3, 4],
+        borderBottom: lang.selected ? '1px solid' : 'none',
+      }}>
+        {lang.langKey.toUpperCase()}
       </li>
     </Link>
   );
 
   return (
-    <ul>
+    <ul sx={{
+      listStyle: 'none',
+      margin: 0,
+    }}>
       {links}
     </ul>
   );
